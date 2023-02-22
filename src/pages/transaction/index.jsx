63852a54cff89470a85e486754746c34ef490126
Alejandro Ortiz <alejandro.ortiz@avoristravel.com>
2023-02-22 17:39:24 +0100

    feat: Introduced Layout for pages.

--- src/pages/transaction/index.jsx
+++ src/pages/transaction/index.jsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import Layout from '../../components/Layout'
 
 export default function Transaction() {
   return (
-    <>
+    <Layout>
       <div className='flex items-center justify-center h-screen'>
         <div className='bg-white p-8 rounded-lg'>
           <h1 className='text-4xl font-bold mb-10'>Transaction</h1>
@@ -12,6 +13,6 @@ export default function Transaction() {
           </div>
         </div>
       </div>
-    </>
+    </Layout>
   )
 }
