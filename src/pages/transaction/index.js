d733135b7e8d78dbbcd1fb6816630df3d991d36f
Alejandro Ortiz <alejandro.ortiz@avoristravel.com>
2023-02-22 17:20:56 +0100

    initial: First commit.

---
+++ src/pages/transaction/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function Transaction() {
+  return (
+    <>
+      <div className='flex items-center justify-center h-screen'>
+        <div className='bg-white p-8 rounded-lg'>
+          <h1 className='text-4xl font-bold mb-10'>Transaction</h1>
+          <div className='border-b mt-8 mb-8' />
+          <div className='text-center text-blue-400 font-bold'>
+            <Link href='/'>Go home</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
